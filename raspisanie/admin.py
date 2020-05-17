3a28d78def0754311f0986f6d2cfc31b429c641d
masson <masson_lesha@mail.ru>
2020-05-18 01:54:08 +0400

    Second step in my DIPLOM

--- raspisanie/admin.py
+++ raspisanie/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Auditory)
+admin.site.register(Day_of_week)
+admin.site.register(Discipline)
+admin.site.register(Group)
+admin.site.register(Pair_number)
+admin.site.register(Teacher)
+admin.site.register(Raspisanie)
+admin.site.register(Changes)
--- raspisanie/models.py
+++ raspisanie/models.py
@@ -1,3 +1,101 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+
+class Day_of_week(models.Model):
+    kod_raspisania = models.AutoField(primary_key = True)
+    day_of_week = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return self.day_of_week
+
+    def publish(self):
+        self.save()
+
+
+
+class Discipline(models.Model):
+    kod_of_discipline = models.AutoField(primary_key = True)
+    name_of_discipline = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.name_of_discipline
+
+    def publish(self):
+        self.save()
+
+
+
+class Teacher(models.Model):
+    kod_of_teacher = models.AutoField(primary_key = True)
+    name = models.CharField(max_length = 50)
+    second_name = models.CharField(max_length = 50)
+    surname = models.CharField(max_length = 50)
+
+    def __str__(self):
+        full_name = self.second_name + ' ' + self.name[0] + '. ' + self.surname[0] + '.'
+        return full_name
+
+    def publish(self):
+        self.save()
+
+
+
+class Group(models.Model):
+    kod_of_group = models.AutoField(primary_key = True)
+    group_name = models.CharField(max_length = 10)
+
+    def __str__(self):
+        return self.group_name
+
+
+
+class Auditory(models.Model):
+    kod_of_auditory = models.AutoField(primary_key = True)
+    number_of_auditory = models.IntegerField()
+
+    def __str__(self):
+        return self.number_of_auditory
+
+
+
+class Pair_number(models.Model):
+    kod_of_pair = models.AutoField(primary_key = True)
+    pair_number = models.IntegerField()
+
+    def __str__(self):
+        return self.pair_number
+
+
+
+class Raspisanie(models.Model):
+    kod_of_raspisanie = models.AutoField(primary_key = True)
+    day_of_week = models.ForeignKey(Day_of_week, on_delete = models.CASCADE)
+    parity = models.BooleanField()
+    discipline = models.ForeignKey(Discipline, on_delete = models.CASCADE)
+    group = models.ForeignKey(Group, on_delete = models.CASCADE)
+    #Данное поле предназначено для заполнения с какой пары начинается дисциплина
+    pair_number = models.ForeignKey(Pair_number, on_delete = models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete = models.CASCADE)
+    auditory = models.ForeignKey(Auditory, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.discipline
+
+
+
+
+class Changes(models.Model):
+    kod_of_raspisanie = models.AutoField(primary_key = True)
+    day_of_week = models.ForeignKey(Day_of_week, on_delete = models.CASCADE)
+    date_of_change = models.DateField()
+    teacher = models.ForeignKey(Teacher, on_delete = models.CASCADE)
+    discipline = models.ForeignKey(Discipline, on_delete = models.CASCADE)
+    group = models.ForeignKey(Group, on_delete = models.CASCADE)
+    auditory = models.ForeignKey(Auditory, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.discipline
+    
