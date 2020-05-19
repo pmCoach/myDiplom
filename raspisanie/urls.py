6fab9c7585b52964514d9563975d1df4d0b1ee6d
masson <masson_lesha@mail.ru>
2020-05-19 18:56:58 +0400

    В данной версии функционируют сраницы для просмотра расписания и замен. В дальнейшем планируется добавление страницы 'Просмотреть расписание на завтра' . На странице 'view_raspisanie.html' на следующем этапе планируется просмотр четности недели. Также в следующей версии будет организовано удаление данных из таблицы 'Changes' и удаление всех данных из таблиц 'Raspisanie' и 'Changes'

--- raspisanie/urls.py
+++ raspisanie/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
 path('', views.group_select, name='home'),
 path('view_raspis/<int:pk>/', views.view_raspis, name='view_raspis'),
-path('view_changes/<int:pk>', views.view_changes, name='view_changes'),
+path('view_changes/<str:gruppa>', views.view_changes, name='view_changes'),
 ]
--- raspisanie/views.py
+++ raspisanie/views.py
@@ -13,10 +13,13 @@ def group_select(request):
 def view_raspis(request, pk):
     groups = get_object_or_404(Group, kod_of_group=pk)
     raspis = Raspisanie.objects.filter(group=groups.kod_of_group)
-    return render(request, 'user_raspisanie/view_raspisanie.html', {'raspis': raspis})
+    group_name = raspis[0]
+    chetnoe = raspis.filter(parity=True)
+    nechetnoe = raspis.filter(parity=False)
+    return render(request, 'user_raspisanie/view_raspisanie.html', {'raspis': raspis, 'group_n': group_name, 'chetnoe': chetnoe, 'nechetnoe': nechetnoe})
 
 
-def view_changes(request, pk):
-    groups = get_object_or_404(Group, kod_of_group=pk)
+def view_changes(request, gruppa):
+    groups = get_object_or_404(Group, group_name=gruppa)
     zamena = Changes.objects.filter(group=groups.kod_of_group)
-    return render(request, 'user_raspisanie/view_changes.html', {})
+    return render(request, 'user_raspisanie/view_changes.html', {'zamena': zamena})
