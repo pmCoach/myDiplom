cdffc468806eb4f173d0833507d0bf1f0aa44817
masson <masson_lesha@mail.ru>
2020-05-24 11:07:37 +0400

    Дипломная программа готова

--- raspisanie/admin.py
+++ raspisanie/admin.py
@@ -1,13 +1,42 @@
 from django.contrib import admin
 from .models import *
 
+class RaspisanieAdmin(admin.ModelAdmin):
+    list_display= ('day_of_week', 'group', 'pair_number', 'parity', 'obe_nedeli', 'discipline', 'group', 'teacher', 'auditory')
+    search_fields = ['group__group_name',]
+    list_filter = ('day_of_week', 'group__group_name')
+
+class ChangesAdmin(admin.ModelAdmin):
+    list_display= ('date_of_change', 'group', 'pair_number', 'discipline', 'teacher', 'auditory' )
+    search_fields = ['group__group_name',]
+
+class AuditoryAdmin(admin.ModelAdmin):
+    list_display= ('number_of_auditory',)
+
+class Day_of_weekAdmin(admin.ModelAdmin):
+    list_display= ('day_of_week', )
+
+class DisciplineAdmin(admin.ModelAdmin):
+    list_display= ('name_of_discipline',)
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display= ('group_name',)
+
+class Pair_numberAdmin(admin.ModelAdmin):
+    list_display= ('pair_number', 'pair_start', 'pair_end')
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display= ('second_name', 'name', 'surname')
+
+class Start_semestr_dateAdmin(admin.ModelAdmin):
+    list_display= ('date',)
 # Register your models here.
-admin.site.register(Auditory)
-admin.site.register(Day_of_week)
-admin.site.register(Discipline)
-admin.site.register(Group)
-admin.site.register(Pair_number)
-admin.site.register(Teacher)
-admin.site.register(Raspisanie)
-admin.site.register(Changes)
-admin.site.register(Start_semestr_date)
+admin.site.register(Auditory,AuditoryAdmin)
+admin.site.register(Day_of_week, Day_of_weekAdmin)
+admin.site.register(Discipline, DisciplineAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Pair_number, Pair_numberAdmin)
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(Raspisanie, RaspisanieAdmin)
+admin.site.register(Changes, ChangesAdmin)
+admin.site.register(Start_semestr_date, Start_semestr_dateAdmin)
--- raspisanie/models.py
+++ raspisanie/models.py
@@ -6,7 +6,11 @@ from django.utils import timezone
 
 class Day_of_week(models.Model):
     kod_raspisania = models.AutoField(primary_key = True)
-    day_of_week = models.CharField(max_length = 20)
+    day_of_week = models.CharField(max_length = 20, verbose_name="День недели")
+
+    class Meta:
+        verbose_name="День недили"
+        verbose_name_plural="Дни недели"
 
     def __str__(self):
         return self.day_of_week
@@ -18,7 +22,11 @@ class Day_of_week(models.Model):
 
 class Discipline(models.Model):
     kod_of_discipline = models.AutoField(primary_key = True)
-    name_of_discipline = models.CharField(max_length = 50)
+    name_of_discipline = models.CharField(max_length = 50, verbose_name="Название дисциплины")
+
+    class Meta:
+        verbose_name="Дисциплину"
+        verbose_name_plural="Дисциплины"
 
     def __str__(self):
         return str(self.name_of_discipline)
@@ -30,9 +38,14 @@ class Discipline(models.Model):
 
 class Teacher(models.Model):
     kod_of_teacher = models.AutoField(primary_key = True)
-    name = models.CharField(max_length = 50)
-    second_name = models.CharField(max_length = 50)
-    surname = models.CharField(max_length = 50)
+    second_name = models.CharField(max_length = 50, verbose_name="Фамилия")
+    name = models.CharField(max_length = 50, verbose_name="Имя")
+    surname = models.CharField(max_length = 50, verbose_name="Отчество")
+
+    class Meta:
+        verbose_name="Преподавателя"
+        verbose_name_plural="Преподаватели"
+
 
     def __str__(self):
         full_name = self.second_name + ' ' + self.name[0] + '. ' + self.surname[0] + '.'
@@ -45,7 +58,11 @@ class Teacher(models.Model):
 
 class Group(models.Model):
     kod_of_group = models.AutoField(primary_key = True)
-    group_name = models.CharField(max_length = 10)
+    group_name = models.CharField(max_length = 10, verbose_name="Группа")
+
+    class Meta:
+        verbose_name="Группу"
+        verbose_name_plural="Группы"
 
     def __str__(self):
         return self.group_name
@@ -54,7 +71,11 @@ class Group(models.Model):
 
 class Auditory(models.Model):
     kod_of_auditory = models.AutoField(primary_key = True)
-    number_of_auditory = models.IntegerField()
+    number_of_auditory = models.IntegerField(verbose_name="Номер аудитории")
+
+    class Meta:
+        verbose_name="Аудиторию"
+        verbose_name_plural="Аудитории"
 
     def __str__(self):
         return str(self.number_of_auditory)
@@ -63,9 +84,13 @@ class Auditory(models.Model):
 
 class Pair_number(models.Model):
     kod_of_pair = models.AutoField(primary_key = True)
-    pair_number = models.IntegerField()
-    pair_start = models.TimeField()
-    pair_end = models.TimeField()
+    pair_number = models.IntegerField(verbose_name="Номер пары")
+    pair_start = models.TimeField(verbose_name="Начало пары")
+    pair_end = models.TimeField(verbose_name="Конец пары")
+
+    class Meta:
+        verbose_name="Номер пары"
+        verbose_name_plural="Номера пар"
 
     def __str__(self):
         return str(self.pair_number)
@@ -73,7 +98,11 @@ class Pair_number(models.Model):
 
 class Start_semestr_date(models.Model):
     kod_of_date = models.AutoField(primary_key = True)
-    date = models.DateField()
+    date = models.DateField(verbose_name="Дата начала семестра")
+
+    class Meta:
+        verbose_name="Дату начала семестра"
+        verbose_name_plural="Дата начала семестра"
 
     def __str__(self):
         data = str(self.date)
@@ -81,31 +110,39 @@ class Start_semestr_date(models.Model):
 
 class Raspisanie(models.Model):
     kod_of_raspisanie = models.AutoField(primary_key = True)
-    day_of_week = models.ForeignKey(Day_of_week, on_delete = models.CASCADE)
-    parity = models.BooleanField()
-    obe_nedeli = models.BooleanField()
-    discipline = models.ForeignKey(Discipline, on_delete = models.CASCADE)
-    group = models.ForeignKey(Group, on_delete = models.CASCADE)
+    day_of_week = models.ForeignKey(Day_of_week, on_delete = models.CASCADE, verbose_name="День недели")
+    parity = models.BooleanField(verbose_name="Четность")
+    obe_nedeli = models.BooleanField(verbose_name="Обе недели")
+    discipline = models.ForeignKey(Discipline, on_delete = models.CASCADE, verbose_name="Дисциплина")
+    group = models.ForeignKey(Group, on_delete = models.CASCADE, verbose_name="Группа")
     #Данное поле предназначено для заполнения с какой пары начинается дисциплина
-    pair_number = models.ForeignKey(Pair_number, on_delete = models.CASCADE)
-    teacher = models.ForeignKey(Teacher, on_delete = models.CASCADE)
-    auditory = models.ForeignKey(Auditory, on_delete = models.CASCADE)
+    pair_number = models.ForeignKey(Pair_number, on_delete = models.CASCADE, verbose_name="Номер пары")
+    teacher = models.ForeignKey(Teacher, on_delete = models.CASCADE, verbose_name="Преподаватель")
+    auditory = models.ForeignKey(Auditory, on_delete = models.CASCADE, verbose_name="Аудитория")
 
     def __str__(self):
         str_new = str(self.day_of_week) + " | Группа: " + str(self.group) + " | " + str(self.discipline) + " | " + str(self.auditory) + " кабинет | Преподаватель: " + str(self.teacher)
         return str_new
+
+    class Meta:
+        verbose_name = 'Пару'
+        verbose_name_plural = 'Расписание'
 
 
 
 
 class Changes(models.Model):
     kod_of_raspisanie = models.AutoField(primary_key = True)
-    date_of_change = models.DateField()
-    pair_number = models.ForeignKey(Pair_number, on_delete = models.CASCADE)
-    teacher = models.ForeignKey(Teacher, on_delete = models.CASCADE)
-    discipline = models.ForeignKey(Discipline, on_delete = models.CASCADE)
-    group = models.ForeignKey(Group, on_delete = models.CASCADE)
-    auditory = models.ForeignKey(Auditory, on_delete = models.CASCADE)
+    date_of_change = models.DateField(verbose_name="Дата замены")
+    pair_number = models.ForeignKey(Pair_number, on_delete = models.CASCADE, verbose_name="Номер пары")
+    teacher = models.ForeignKey(Teacher, on_delete = models.CASCADE, verbose_name="Преподаватель")
+    discipline = models.ForeignKey(Discipline, on_delete = models.CASCADE, verbose_name="Дисциплина")
+    group = models.ForeignKey(Group, on_delete = models.CASCADE, verbose_name="Группа")
+    auditory = models.ForeignKey(Auditory, on_delete = models.CASCADE, verbose_name="Аудитория")
+
+    class Meta:
+        verbose_name="Замену"
+        verbose_name_plural="Замены"
 
 
     def __str__(self):
